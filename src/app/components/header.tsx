929ae8539db821c78db50903c763df956d64c02d
lucas.lopes <lucasmendeslopes.pessoal@gmail.com>
2024-05-31 14:50:14 -0300

    feat: mobile navbar

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,4 +1,10 @@
+'use client'
+
+import { useState } from 'react'
+
 export function Header() {
+  const [isOpen, setIsOpen] = useState(false)
+
   return (
     <header className="sticky top-0 z-50 flex h-12 w-full items-center justify-center bg-[#1C1C1C] shadow-sm md:h-20">
       <div className="container items-center">
@@ -6,11 +12,49 @@ export function Header() {
           LOGO
         </a>
 
-        <nav className="md:hidden">
-          <button className="flex size-4 items-center justify-center rounded-sm border border-white text-center">
-            <p className="text-white">...</p>
+        <div className="md:hidden">
+          <button
+            className="flex size-6 flex-col items-center justify-between rounded-sm border border-white p-1"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <span className="h-[.125rem] w-full rounded-sm bg-white"></span>
+            <span className="h-[.125rem] w-full rounded-sm bg-white"></span>
+            <span className="h-[.125rem] w-full rounded-sm bg-white"></span>
           </button>
-        </nav>
+
+          <nav
+            className={`absolute left-0 top-12 flex transition-all duration-200 ease-out [&>*]:transition-all ${isOpen ? 'h-96 [&>*]:opacity-100' : 'h-0 p-0 [&>*]:opacity-0'} w-full flex-col items-center justify-between gap-6 bg-[#1C1C1C] p-3 font-sans text-lg font-semibold text-white`}
+            onClick={() => setIsOpen(false)}
+          >
+            <a
+              href="#about"
+              className="inline-block after:block after:origin-left after:scale-x-0 after:border-b-2 after:border-[#8685EF] after:transition-transform after:content-[''] hover:after:scale-x-100"
+            >
+              Sobre
+            </a>
+
+            <a
+              href="#tecnologies"
+              className="inline-block after:block after:origin-left after:scale-x-0 after:border-b-2 after:border-[#8685EF] after:transition-transform after:content-[''] hover:after:scale-x-100"
+            >
+              Tecnologias
+            </a>
+
+            <a
+              href="#projects"
+              className="inline-block after:block after:origin-left after:scale-x-0 after:border-b-2 after:border-[#8685EF] after:transition-transform after:content-[''] hover:after:scale-x-100"
+            >
+              Projetos
+            </a>
+
+            <a
+              href="#contact"
+              className="inline-block after:block after:origin-left after:scale-x-0 after:border-b-2 after:border-[#8685EF] after:transition-transform after:content-[''] hover:after:scale-x-100"
+            >
+              Contato
+            </a>
+          </nav>
+        </div>
 
         <nav className="hidden gap-6 font-sans text-lg font-semibold text-white md:flex">
           <a
