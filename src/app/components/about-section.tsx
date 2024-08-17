c914086c61ff56d1c7d8ba672cad0dd4d1862690
lucas.lopes <lucasmendeslopes.pessoal@gmail.com>
2024-08-17 12:18:15 -0300

    feat: changes to the text of the about section

--- src/app/components/about-section.tsx
+++ src/app/components/about-section.tsx
@@ -46,23 +46,20 @@ export function AboutSection() {
             >
               <p className="text-white">
                 Com 3 anos de experiência no mercado de programação, construí
-                uma trajetória sólida e diversificada. Iniciei minha carreira na
+                uma trajetória sólida e diversificada.
+              </p>
+
+              <p className="text-white">Iniciei minha carreira na
                 Digital Business, onde fui responsável pelo desenvolvimento de
                 páginas web responsivas e animações publicitárias para
                 multinacionais, dentre elas, VW, GM, Nestlé, Ferrero. Esse
                 trabalho inicial me proporcionou uma base robusta em Frontend,
                 permitindo-me adquirir habilidades essenciais e uma compreensão
                 profunda das melhores práticas para a criação de interfaces
-                atrativas e eficazes.
-              </p>
+                atrativas e eficazes.</p>
 
               <p className="text-white">
-                Atualmente, integro a equipe da Paipe Tecnologia e Inovação,
-                onde continuo a expandir minhas competências e contribuir para o
-                desenvolvimento de aplicações web inovadoras. Tenho orgulho de
-                estar envolvido em projetos desafiadores que me permitem aplicar
-                e aprimorar meus conhecimentos continuamente, sempre buscando
-                superar as expectativas e entregar soluções de alta qualidade.
+                Após, integrei a equipe da Paipe Tecnologia e Inovação, onde desenvolvi sistemas para diferentes ramos e organizações, usando responsividade, consumo de APIs REST e tecnologias tais como JavaScript, TypeScript, React JS, Next JS, Context API, Styled Components, Material-UI (MUI), Scrum, Kanban, Git, GitLab, Trello e Jira.
               </p>
 
               <p className="text-white">
